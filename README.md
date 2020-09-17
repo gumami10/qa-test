fb39d7cba14a299cf6b231b9229f739d9676019b
Eric Groppe <emmgp@hotmail.com>
2020-09-17 00:16:28 -0300

    test persistance for goomer test

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Cypress com Typescript
 
 BoilerPlate de um projeto de cypress com os testes escritos em Typescript,
-com exemplos do próprio cypress e alguns feitos para o Jobzz
+com exemplos do próprio cypress e alguns feitos para o teste da Goomer
+
+# Metodologia
+
+Os testes foram baseados no método ATDD, em que os casos de teste são baseados nos
+critérios de aceite.
 
 ## Instalação
 
---
+++ cypress/integration/e2e/todo-persistent.spec.ts
@@ -0,0 +1,38 @@
+/// <reference types="cypress"/>
+
+context('todo page persistence', () => {
+  beforeEach(() => {
+    cy.visit(Cypress.env('host'))
+  })
+
+  it('created todos should persist', () => {
+    cy.addTodo('Aldair pereira o grandioso')
+
+    cy.visit(Cypress.env('host'))
+
+    cy.assertHowManyTodos(1)
+
+    cy.get('body').should(() => {
+
+      expect(localStorage.getItem('todos-vanillajs')).to.be.not.null
+    })
+
+  })
+
+  it('completed todos should persist', () => {
+    cy.addTodo('Aldair pereira o grandioso')
+
+    cy.get('.toggle').check()
+    cy.get('.toggle').should('be.checked')
+
+    cy.visit(Cypress.env('host'))
+
+    cy.assertHowManyTodos(1)
+
+    cy.get('body').should(() => {
+
+      expect(localStorage.getItem('todos-vanillajs')).to.be.not.null
+    })
+
+  })
+})
